5375f21b3bc124085b5a5de7879a91f510d329df
Cédric Ronvel <cedric.ronvel@gmail.com>
2022-02-22 20:12:16 +0100

    wip

--- sample/throttle.js
+++ sample/throttle.js
@@ -6,23 +6,23 @@ const term = require( 'terminal-kit' ).terminal ;
 
 
 var motor = new physic.dynamics.Motor( {
-	torquePerRpm: [
+	torqueData: [
 		{ rpm: 0 , torque: 30 } ,
 		{ rpm: 1000 , torque: 100 } ,
 		{ rpm: 2000 , torque: 120 } ,
-		{ rpm: 4000 , torque: 200 } ,
+		{ rpm: 3500 , torque: 200 } ,
+		{ rpm: 5000 , torque: 300 } ,
+		{ rpm: 5500 , torque: 350 } ,
 		{ rpm: 6000 , torque: 300 } ,
-		{ rpm: 6500 , torque: 350 } ,
-		{ rpm: 7000 , torque: 300 } ,
-		{ rpm: 8000 , torque: 250 } ,
-		{ rpm: 9000 , torque: 200 } ,
-		{ rpm: 10000 , torque: 100 } ,
-		{ rpm: 12000 , torque: 0 } ,
+		{ rpm: 7500 , torque: 250 } ,
+		{ rpm: 8500 , torque: 150 } ,
+		{ rpm: 9000 , torque: 100 } ,
+		{ rpm: 10000 , torque: 0 } ,
 		{ rpm: 20000 , torque: -200 }
 	] ,
 	brakeTorquePerRpm: 0.08 ,
 	brakeMinRpm: 1000 ,
-	motorInertia: 0.4 ,
+	motorInertia: 4 ,
 } ) ;
 
 // Listen for move events on all gamepads
@@ -44,7 +44,7 @@ term.bold.green( "Move the mouse upward or downward to change the motor throttle
 function update() {
 	motor.apply( 0.1 ) ;
 	term.column( 1 ).eraseLineAfter() ;
-	term.bold.yellow( "%i RPM (throttle: %P, torque: %[.2]f)" , motor.rpm , motor.throttle , motor.torque ) ;
+	term.bold.yellow( "%[L5]i RPM => %[L4]P | %[L6]kN.m | %[L6]kW / %[L4]ihp | %[4L6]f rad/s" , motor.rpm , motor.throttle , motor.torque , motor.power , motor.horsePower , motor.angularSpeed ) ;
 }
 
 setInterval( update , 100 ) ;
