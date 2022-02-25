dcb884a927745702fc9aead546962219dc86060d
Cédric Ronvel <cedric.ronvel@gmail.com>
2022-02-25 20:26:11 +0100

    wip

--- sample/spring-damper.js
+++ sample/spring-damper.js
@@ -5,8 +5,9 @@ const term = require( 'terminal-kit' ).terminal ;
 const math = require( 'math-kit' ) ;
 
 
+
 // Parameters/starting conditions
-var timeStep = 0.1 ,
+var timeStep = 0.05 ,
 	maxTime = 20 ,
 	mass = 10 ,
 	springElasticity = 200 ,
@@ -14,7 +15,17 @@ var timeStep = 0.1 ,
 	dampingFactor = 200 ,
 	displacement = -0.75 ,
 	velocity = 0 ,
-	externalForce = -98 ;
+	externalForce = -98 ,
+	useBrakeAlgo = false ;
+
+/*
+	Brake algo is fine for 1D, but does not works in 3D except if each individual braking forces are stored and applied
+	one after the other, and use dot product for each indiviual “braking-acceleration”.
+	It doesn't works if all brakes are summed.
+	E.g.: Consider a ball at the very bouncing moment, it has F air drag in the -velocity direction and a F ground drag
+	on the ground plane. When closed to right angle, the F ground drag may well revert the xy velocity (z-up), because
+	when summed with the F air drag the dot product is positive.
+*/
 
 var springK = springElasticity / springRestLength ,
 	time = 0 ,
@@ -26,26 +37,28 @@ var springK = springElasticity / springRestLength ,
 
 
 function computeForces() {
-	force = externalForce ;
-	//force = externalForce - displacement * springK - dampingFactor * velocity ;
-	brakingForce = - displacement * springK - dampingFactor * velocity ;
-	acceleration = force / mass ;
-	brakingAcceleration = brakingForce / mass ;
+	if ( useBrakeAlgo ) {
+		force = externalForce - displacement * springK ;
+		acceleration = force / mass ;
+		brakingForce = - dampingFactor * velocity ;
+		brakingAcceleration = brakingForce / mass ;
+	}
+	else {
+		force = externalForce - displacement * springK - dampingFactor * velocity ;
+		acceleration = force / mass ;
+	}
 }
 
 
 
-function updateVerlet( dt ) {
-	displacement += velocity * dt + 0.5 * acceleration * dt *dt ;
+function updateVerletBrakeAlgo( dt ) {
+	displacement += velocity * dt + 0.5 * ( acceleration + brakingAcceleration ) * dt *dt ;
 	var lastAcceleration = acceleration ;
 	var lastBrakingAcceleration = brakingAcceleration ;
 	computeForces() ;
 	velocity += 0.5 * ( lastAcceleration + acceleration ) * dt ;
 	brake = 0.5 * ( lastBrakingAcceleration + brakingAcceleration ) * dt ;
 
-	velocity += brake ;
-
-	/*
 	// Check if the braking force would create a reverse velocity artifact, if so, nullify the velocity now
 	if ( velocity && brake ) {
 		// Check if the sign changes after the brake
@@ -55,8 +68,18 @@ function updateVerlet( dt ) {
 		else {
 			velocity = 0 ;
 		}
-	}*/
+	}
 
+	time += dt ;
+}
+
+
+
+function updateVerlet( dt ) {
+	displacement += velocity * dt + 0.5 * acceleration * dt *dt ;
+	var lastAcceleration = acceleration ;
+	computeForces() ;
+	velocity += 0.5 * ( lastAcceleration + acceleration ) * dt ;
 	time += dt ;
 }
 
@@ -71,7 +94,7 @@ function tracerReport() {
 		width: 1000 ,
 		height: 800 ,
 		bgColor: '#000' ,
-		xmin: -0.5 ,
+		xmin: -1 ,
 		xmax: maxTime ,
 		ymin: -1.5 ,
 		ymax: 1.5 ,
@@ -92,7 +115,12 @@ function tracerReport() {
 
 
 function frameTextReport() {
-	term.yellow( "T: %[.3]fs => d:%[.3]fm v:%[.3]fm/s a:%[.3]fm/s²\n" , time , displacement , velocity , acceleration ) ;
+	if ( useBrakeAlgo ) {
+		term.yellow( "T: %[.3]fs => d:%km v:%km/s a:%km/s² brkA:%km/s²\n" , time , displacement , velocity , acceleration , brakingAcceleration ) ;
+	}
+	else {
+		term.yellow( "T: %[.3]fs => d:%km v:%km/s a:%km/s²\n" , time , displacement , velocity , acceleration ) ;
+	}
 }
 
 
@@ -103,7 +131,13 @@ async function run() {
 	frameTextReport() ;
 
 	while ( time <= maxTime ) {
-		updateVerlet( timeStep ) ;
+		if ( useBrakeAlgo ) {
+			updateVerletBrakeAlgo( timeStep ) ;
+		}
+		else {
+			updateVerlet( timeStep ) ;
+		}
+
 		graphData.push( { x: time , fx: displacement } ) ;
 		frameTextReport() ;
 	}
