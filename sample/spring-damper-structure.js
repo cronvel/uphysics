215675a74095ff48c77047f8842f30ae61ec20f4
Cédric Ronvel <cedric.ronvel@gmail.com>
2022-03-04 21:54:00 +0100

    SpringDamperStructure builder

---
+++ sample/spring-damper-structure.js
@@ -0,0 +1,129 @@
+
+"use strict" ;
+
+const physic = require( '..' ) ;
+const math = require( 'math-kit' ) ;
+
+var isElectron , term ;
+
+if ( process.type === 'renderer' ) {
+	isElectron = true ;
+}
+else {
+	isElectron = false ;
+	term = require( 'terminal-kit' ).terminal ;
+}
+
+
+
+var timeStep = 0.01 ,
+	maxTime = 4 ,
+	//integrator = 'euler' ,
+	integrator = 'verlet' ,
+	//integrator = 'predictor' ,
+	//integrator = 'predictorUnstiffed' ,
+	springK = 1000 ,
+	dampingFactor = 50 ,
+	restLength = 1 ,
+	mass = 10 ,
+	initialVY = 5 ;
+
+
+
+var world = new physic.World( {
+	timeStep: timeStep ,
+	integrator: integrator
+} ) ;
+
+
+
+var planeShape = physic.Shape.createPlane( new physic.Vector3D( 0 , 1 , 0 ) ) ;
+var basicMaterial = new physic.Material( { isSolid: true } ) ;
+basicMaterial.setSelfInteraction( new physic.MaterialInteraction( { hq: true } ) ) ;
+
+var gravity = world.createDynamic( physic.dynamics.ConstantAcceleration , { acceleration: new physic.Vector3D( 0 , -9.8 , 0 ) } ) ;
+
+var structureTemplate = new physic.builders.SpringDamperStructure( {
+	dynamics: [ gravity ] ,
+	size: new physic.Vector3D( 1 , 1 , 2 ) ,
+	material: basicMaterial ,
+	springK , dampingFactor , mass ,
+} ) ;
+
+var structure = structureTemplate.build( world , new physic.Vector3D( 0 , 2.5 , 0 ) ) ;
+
+//term( "Entity count: %i\nDynamic count: %i\n" , structure.entities.length , structure.dynamics.length ) ;
+//return ;
+
+var plane = world.createEntity( {
+	id: 'static-plane' ,
+	shape: planeShape ,
+	material: basicMaterial ,
+	isStatic: true
+} ) ;
+
+
+
+function init() { world.computeInitialForces() ; }
+function update() { world.update() ; }
+
+
+
+async function runTerminal() {
+	init() ;
+	frameTextReport() ;
+
+	while ( world.time < maxTime ) {
+		update() ;
+		frameTextReport() ;
+	}
+}
+
+
+
+function frameTextReport() {
+	term.yellow( "--- T: %[.3]fs --\n" , world.time ) ;
+	frameTextReportEnt( structure.entities[ 0 ] ) ;
+}
+
+
+
+function frameTextReportEnt( ent ) {
+	if ( ent.position.y < -0.0001 ) {
+		term.red( "\t%s pos:(%[.3]f,%f,%[.3]f) vel:(%[.3]f,%[.3]f,%[.3]f) accel:(%[.3]f,%[.3]f,%[.3]f)\n" ,
+			ent.id ,
+			ent.position.x , ent.position.y , ent.position.z , 
+			ent.velocity.x , ent.velocity.y , ent.velocity.z , 
+			ent.acceleration.x , ent.acceleration.y , ent.acceleration.z
+		) ;
+		process.exit() ;
+		return ;
+	}
+
+	if ( ent.position.y <= 0 ) {
+		term.brightYellow( "\t%s pos:(%[.3]f,%f,%[.3]f) vel:(%[.3]f,%[.3]f,%[.3]f) accel:(%[.3]f,%[.3]f,%[.3]f)\n" ,
+			ent.id ,
+			ent.position.x , ent.position.y , ent.position.z , 
+			ent.velocity.x , ent.velocity.y , ent.velocity.z , 
+			ent.acceleration.x , ent.acceleration.y , ent.acceleration.z
+		) ;
+		return ;
+	}
+	
+
+	term.yellow( "\t%s pos:(%[.3]f,%[.3]f,%[.3]f) vel:(%[.3]f,%[.3]f,%[.3]f) accel:(%[.3]f,%[.3]f,%[.3]f)\n" ,
+		ent.id ,
+		ent.position.x , ent.position.y , ent.position.z , 
+		ent.velocity.x , ent.velocity.y , ent.velocity.z , 
+		ent.acceleration.x , ent.acceleration.y , ent.acceleration.z
+	) ;
+}
+
+
+
+exports.world = world ;
+exports.init = init ;
+exports.update = update ;
+
+if ( ! isElectron ) { runTerminal() ; }
+
